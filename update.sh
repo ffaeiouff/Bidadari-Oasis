0b03b358baa3c5b3f02dd30a922795fd3c92390a
Soedarsono <soedar.sg@gmail.com>
2016-03-05 09:38:49 +0800

    Added a live flag for update script

--- update.sh
+++ update.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+if [ "$LIVE" = "true" ]; then
+    git fetch --all
+    git reset --hard origin/master
+    docker build -t hdb_scraper .
+fi
+
 rm -f data/bidadari.log
+
 docker run --rm -v `pwd`/data:/app/data hdb_scraper
+
+cat data/bidadari.log
 
 # If data scrape is ok
 if grep --quiet "^###OK###$" data/bidadari.log; then
@@ -9,6 +18,9 @@ if grep --quiet "^###OK###$" data/bidadari.log; then
     if [[ $(git diff data/bidadari.csv data/bidadari.json) ]]; then
         git add data/
         git commit -m "Updated data on `date`"
+        if [ "$LIVE" = "true" ]; then
+            git push
+        fi
     else
         echo "No change in data"
         git checkout -- data/
